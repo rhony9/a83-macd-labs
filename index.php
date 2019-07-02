6d72bbe9490e996df0d42aefe1feb4c19ae50e71
Rhon <rseptian8@gmail.com>
2019-07-03 04:39:25 +0700

    Change env

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "azure-course.database.windows.net";
+    $user = "azure";
+    $pass = "Course123@";
+    $db = "azure-course";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
